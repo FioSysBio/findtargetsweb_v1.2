0ba0a5d643b939a2a70cf31c5e463655a863a2d5
Thiago Giannini Ramos <gianninimail@gmail.com>
2023-06-30 14:54:30 -0300

    Add path sistema in static files

--- findtargetsweb/settings.py
+++ findtargetsweb/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'findtargetsweb/static')
+    "/sistemas/findtargetsweb_v1.2/static"
+    # os.path.join(BASE_DIR, 'findtargetsweb/static')
 ]
 
 
